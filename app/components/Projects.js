af022e4bd561a1650a6aa4efac7c4335900e5265
Cody Boaz <codyryanboaz@yahoo.com>
2020-01-29 17:42:48 -0800

    added hacker news project

--- app/components/Projects.js
+++ app/components/Projects.js
@@ -4,7 +4,7 @@ import yelpCampImg from '../images/yelpCamp.png'
 import budgetAppImg from '../images/budget.png'
 import diceGameImg from '../images/dice.png'
 import colorGameImg from '../images/colorGame.png'
-import favoriteMovieImg from '../images/favoriteMovies.png'
+import hackerNews from '../images/hacker-news.png'
 
 export default function Projects() {
   return (
@@ -16,6 +16,12 @@ export default function Projects() {
         <a href='https://wonderful-shockley-b7c15a.netlify.com/'>
           <img src={githubBattleImg} alt='Github Battle Website' className='proj-img' />
           <p>Github Battle</p>
+        </a>
+      </div>
+      <div className='project-img'>
+        <a href='https://hacker-news-1214.netlify.com/'>
+          <img src={hackerNews} alt='Hacker News Clone Website' className='proj-img' />
+          <p>Hacker News Clone</p>
         </a>
       </div>
       <div className='project-img'>
@@ -40,12 +46,6 @@ export default function Projects() {
         <a href='https://codyboaz.github.io/rgb_game/'>
           <img src={colorGameImg} alt='Color Game App' className='proj-img' />
           <p>Color Game</p>
-        </a>
-      </div>
-      <div className='project-img'>
-        <a href='https://codyboaz.github.io/favorite-movie-website/'>
-          <img src={favoriteMovieImg} alt='Favorite Movies Website' className='proj-img' />
-          <p>Favorite Movies Website</p>
         </a>
       </div>
     </div>
